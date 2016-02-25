03a09573d79c66d55c6fb50a740db90028ca146c
sukanyabhat <sukanyabhat@users.noreply.github.com>
2016-02-24 20:54:46 -0700

    Update timeseries.py

--- timeseries.py
+++ timeseries.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Feb 24 00:46:37 2016
-
-@author: sukanyabhat
-"""
 import pandas as pd
 #numpy for numerical manipulation
 #import numpy as np
@@ -17,7 +11,7 @@ def timeseries(ts):
     #taking span of 30 since it is daily data
     rolmean = pd.rolling_mean(ts, window=30)
     rolstd = pd.rolling_std(ts, window=30)
-    
+      
     #Plot rolling statistics:
     plt.plot(ts, color='blue',label='Original')
     plt.plot(rolmean, color='red', label='Rolling Mean')
@@ -25,12 +19,21 @@ def timeseries(ts):
     plt.legend(loc='best')
     plt.title('Rolling Mean & Standard Deviation')
     plt.show(block=False)
-    
-    #Perform Dickey-Fuller test:
-    print ('Results of Dickey-Fuller Test:')
-    dftest = adfuller(ts, autolag='AIC')
-    dfoutput = pd.Series(dftest[0:4], index=['Test Statistic','p-value','#Lags Used','Number of Observations Used'])
-    for key,value in dftest[4].items():
-        dfoutput['Critical Value (%s)'%key] = value
-    print (dfoutput)
 
+    events= []
+
+    for key in ts.keys():
+        if ts[key] > rolmean[key]+3*rolstd[key]:
+            events.append(key)
+    for key in ts.keys():
+        if ts[key] < rolmean[key]-2*rolstd[key]:
+            events.append(key)
+            
+    return events
+    #Perform Dickey-Fuller test:
+#    print ('Results of Dickey-Fuller Test:')
+#    dftest = adfuller(ts, autolag='AIC')
+#    dfoutput = pd.Series(dftest[0:4], index=['Test Statistic','p-value','#Lags Used','Number of Observations Used'])
+#    for key,value in dftest[4].items():
+#        dfoutput['Critical Value (%s)'%key] = value
+#        print (dfoutput)
