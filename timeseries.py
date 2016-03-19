e439cfd5a2e6b5b4bbc574b395b907fc01aa1942
Jo Tang <htang37@users.noreply.github.com>
2016-03-19 16:12:22 -0700

    03/19/2016_ARIMA
    today's changes with auto.arima

--- timeseries.py
+++ timeseries.py
@@ -1,35 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 19 14:45:45 2016
+
+@author: JokerSoup
+"""
+
 import pandas as pd
 #numpy for numerical manipulation
 #import numpy as np
 #to plot
 import matplotlib.pylab as plt
+from rpy2 import *
+import rpy2.robjects as RO
+from rpy2.robjects import globalenv
 
 #for adjusted dickey fuller test
 from statsmodels.tsa.stattools import adfuller
-def timeseries(ts):
-    plt.plot(ts)
+
+def timeseries(abc):
+    plt.plot(abc)
     #taking span of 30 since it is daily data
-    rolmean = pd.rolling_mean(ts, window=30)
-    rolstd = pd.rolling_std(ts, window=30)
+    rolmean = pd.rolling_mean(abc, window=30)
+    rolstd = pd.rolling_std(abc, window=30)
       
     #Plot rolling statistics:
-    plt.plot(ts, color='blue',label='Original')
+    plt.plot(abc, color='blue',label='Original')
     plt.plot(rolmean, color='red', label='Rolling Mean')
     plt.plot(rolstd, color='black', label = 'Rolling Std')
     plt.legend(loc='best')
     plt.title('Rolling Mean & Standard Deviation')
     plt.show(block=False)
 
+    globalenv['abc'] = abc
+    RO.r('fit <- auto.arima(ts(abc,frequency=7))')
+    RO.r('print(fit)')
     events= []
-
-    for key in ts.keys():
-        if ts[key] > rolmean[key]+3*rolstd[key]:
+    
+    
+    for key in abc.keys():
+        if abc[key] > rolmean[key]+3*rolstd[key]:
             events.append(key)
-    for key in ts.keys():
-        if ts[key] < rolmean[key]-2*rolstd[key]:
+    for key in abc.keys():
+        if abc[key] < rolmean[key]-2*rolstd[key]:
             events.append(key)
-            
-    return events
+ 
+    return events           
+    
     #Perform Dickey-Fuller test:
 #    print ('Results of Dickey-Fuller Test:')
 #    dftest = adfuller(ts, autolag='AIC')
